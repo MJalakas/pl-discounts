fd69dcd4ceee602e2c72fb079b2253dd9d4db622
martenj <marten@jalakas.ee>
2024-11-12 20:06:19 +0200

    Add style for main content layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import { Roboto } from "next/font/google";
 import MainHeader from "@/app/components/MainHeader";
 
 export const metadata = {
-    title: "Piletilevi Back-office",
+    title: "Piletilevi Back Office",
     description: "Only for demo purposes.",
 };
 
@@ -15,10 +15,11 @@ const roboto = Roboto({
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en" className={`${roboto.className} text-[#1A1A1A]`}>
-            <body className="w-full h-full min-h-[100dvh]">
+        <html lang="en" className={`${roboto.className} text-[#1A1A1A] bg-[#F5F5F7]`}>
+            <body className="w-full h-[100dvh] min-h-[100dvh]">
                 <MainHeader />
-                <main className="bg-[#E3E3E3] w-full h-full">{children}</main>
+                {/* 5rem in calc is the header height. This way main content will always take up the rest of the screen without a scrollbar by default. */}
+                <main className="px-32 py-4 w-full min-h-[calc(100%-5rem)] ">{children}</main>
             </body>
         </html>
     );
